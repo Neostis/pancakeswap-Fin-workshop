db96ff751dae78a36c775cf6285b7df703cc51fb
Neostis <jeerapat.som@gmail.com>
2022-07-28 16:25:18 +0700

    update addliquidity

--- pages/addliquidity.tsx
+++ pages/addliquidity.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const addliquidity = () => {
   return (
-    <div>addliquidity</div>
+    <div className="bg-blue-300 place-items-center ">
+      <div className="  bg-blue-100 max-w-xs place-items-center ">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+        Liquidity
+        </label>
+        <h1><button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 border border-blue-700">
+        Add Liquidity
+  </button></h1>
+
+        <h3><label className="block text-gray-700 text-sm font-bold mb-2">
+        Your Liquidity
+
+        </label></h3>
+
+        <h3><label className="bg-blue-600 shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline">
+                     
+        </label></h3>
+  </div>
+</div>
   )
 }
 

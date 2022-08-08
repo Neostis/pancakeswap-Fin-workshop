d1e29174462dcc1284021a2806ed51ee2ac8b156
Neostis <jeerapat.som@gmail.com>
2022-08-08 11:38:56 +0700

    fix popup

--- components/popup.tsx
+++ components/popup.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import * as ethers from "ethers";
+import * as ethers from 'ethers';
 import {
   connectWallet,
   getWalletAddress,
@@ -8,20 +8,16 @@ import {
   getEthereum,
   getBalance,
   getProvider,
-} from "../services/wallet-service";
-import {
-  getNetworkName,
-  getNetworkCurrency,
-  getNetworkTokens,
-} from "../constants/network-id";
-import { formatEther, formatUnits } from "ethers/lib/utils";
-import { Token } from "../types/token.type";
-import { ETH_TOKENS } from "../constants/tokens";
+} from '../services/wallet-service';
+import { getNetworkName, getNetworkCurrency, getNetworkTokens } from '../constants/network-id';
+import { formatEther, formatUnits } from 'ethers/lib/utils';
+import { Token } from '../types/token.type';
+import { ETH_TOKENS, RINKEBY_TOKENS, KOVAN_TOKENS } from '../constants/tokens';
 
 export default function popup() {
   const [showModal, setShowModal] = useState(true);
-  const [address, setAddress] = useState<string| null>(null);
-  const [network, setNetwork] = useState<string| null>(null);
+  const [address, setAddress] = useState<string | null>(null);
+  const [network, setNetwork] = useState<string | null>(null);
   const loadAccountData = async () => {
     const addr = getWalletAddress();
     setAddress(getWalletAddress());
@@ -60,41 +56,48 @@ export default function popup() {
       setNetwork(networkId);
     };
 
-    getEthereum()?.on("accountsChanged", handleAccountChange);
+    getEthereum()?.on('accountsChanged', handleAccountChange);
     // window.ethereum.on('accountsChanged', (address: string[]) => {
     //     setAddress(address[0]);
     // });
-    getEthereum()?.on("chainChanged", handleNetworkChange);
+    getEthereum()?.on('chainChanged', handleNetworkChange);
     // window.ethereum.on('chainChanged', (networkId: string) => {
     //     setNetwork(networkId);
     // });
     //
   }, []);
+
+  // {/* <button
+  //       className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+  //       type="button"
+  //       onClick={() => setShowModal(true)}
+  //     >
+  //       {/* Open regular modal */}
+  //     </button> */}
   return (
     <>
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        {/* Open regular modal */}
-      </button>
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="bg-textwhite border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <Dropdown className="d-inline mx-2">
+                <div className="">
+                  {/* <select className="d-inline mx-2" color="blue" id="list-token1"> */}
+                  {/* ETH_TOKENS.map((e) => {<option value={e.address} > </option> }); */}
+                  <option value={ETH_TOKENS[0].address}>{ETH_TOKENS[0].symbol}</option>
+                  <option value={ETH_TOKENS[1].address}>{ETH_TOKENS[1].symbol}</option>
+                  <option value={ETH_TOKENS[2].address}>{ETH_TOKENS[2].symbol}</option>
+                  {/* </select> */}
+                  {/* <Dropdown className="d-inline mx-2">
                     <Dropdown.Toggle id="dropdown-autoclose-true">Default Dropdown</Dropdown.Toggle>
                     <Dropdown.Menu>
-                      {/* <Dropdown.Item href="#">Menu Item</Dropdown.Item>
                       <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                      <Dropdown.Item href="#">Menu Item</Dropdown.Item> */}
+                      <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+                      <Dropdown.Item href="#">Menu Item</Dropdown.Item>
                     </Dropdown.Menu>
-                  </Dropdown>
+                  </Dropdown> */}
                   {/* <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}

c6c32f903a83080e8f142218f4d15486a5c20792
KritzHeng <mr.kriskanin@gmail.com>
2022-08-08 15:10:48 +0700

    search input

--- components/popup.tsx
+++ components/popup.tsx
@@ -21,6 +21,7 @@ import { ETH_TOKENS } from "../constants/tokens";
 export default function popup() {
   const [showModal, setShowModal] = useState(true);
   const [address, setAddress] = useState<string | null>(null);
+  const [token1, setToken1] = useState<string | null>(null);
   const [network, setNetwork] = useState<string | null>(null);
   const loadAccountData = async () => {
     const addr = getWalletAddress();
@@ -73,6 +74,13 @@ export default function popup() {
     // });
     //
   }, []);
+
+
+
+  const showToken = () => {
+
+    console.log(token1);
+  }
   return (
     <>
       <button
@@ -87,44 +95,36 @@ export default function popup() {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
+                  <input placeholder='search token'></input>
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <Dropdown className="d-inline mx-2">
-                    <Dropdown.Toggle id="dropdown-autoclose-true">Default Dropdown</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      {/* <Dropdown.Item href="#">afas</Dropdown.Item> */}
-                      {
-                        ETH_TOKENS.map((e) => {
-                          return (
-                            <div key={e.address}>
+                  {
+                    ETH_TOKENS.map((e) => {
+                      return (
+                        <div key={e.address}>
+                          <div className='flex'>
+                            <img src={e.imageUrl} width="100" height="150"></img>
+                            <p>{e.symbol}</p>
+                          </div>
 
-                              <Dropdown.Item ><img src={e.imageUrl}></img>{e.symbol}</Dropdown.Item>
-                            </div>
+                          {/* <Dropdown.Item value={e.symbol}
+        onChange={(a) => setToken1(a.target.value)}><img src={e.imageUrl}></img>{e.symbol}</Dropdown.Item> */}
+                        </div>
 
-                          )
-                        }
-                        )
-                      }
+                      )
+                    }
+                    )
+                  }
 
-                      {/* <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                      <Dropdown.Item href="#">Menu Item</Dropdown.Item> */}
-                    </Dropdown.Menu>
-                  </Dropdown>
-                  {/* <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button> */}
+
+
+
+
+
+
                 </div>
-                {/*body*/}
-                {/* <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">TEXT here bobo</p>
-                </div> */}
-                {/*footer*/}
+
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
